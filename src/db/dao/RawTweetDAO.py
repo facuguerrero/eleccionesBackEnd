f4c3dcbc5c7d3ffb542c78799f8b1aa7425f645f
Facundo Guerrero <facundo.guerrero@despegar.com>
2019-06-05 18:47:26 -0300

    removes logger

--- src/db/dao/RawTweetDAO.py
+++ src/db/dao/RawTweetDAO.py
@@ -19,5 +19,5 @@ class RawTweetDAO(GenericDAO, metaclass=Singleton):
         try:
             self.insert(raw_tweet)
         except DuplicateKeyError as error:
-            self.logger.warning(f'Trying to insert a duplicated tweet {raw_tweet["user_id"]}.')
+            #self.logger.warning(f'Trying to insert a duplicated tweet {raw_tweet["user_id"]}.')
             raise DuplicatedTweetError
