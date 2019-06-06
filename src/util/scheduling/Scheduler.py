448e0e29b61c262bc1b0c837516536ff2edb76aa
Facundo Guerrero <facundo.guerrero@despegar.com>
2019-06-06 09:03:05 -0300

    modifies alert

--- src/util/scheduling/Scheduler.py
+++ src/util/scheduling/Scheduler.py
@@ -15,6 +15,6 @@ class Scheduler(metaclass=Singleton):
         """ Configure scheduler's jobs. """
         # Execute at 00:00:00 every day
         self.scheduler.add_job(func=FollowerUpdateService.update_followers, trigger='cron', hour=0, minute=0, second=0)
-        self.scheduler.add_job(func=SlackHelper.send_server_status, trigger='cron', hour=9, minute=30, second=0)
+        self.scheduler.add_job(func=SlackHelper.send_server_status, trigger='cron', hour=8, minute=30, second=0)
         self.scheduler.start()
         atexit.register(lambda: self.scheduler.shutdown())
