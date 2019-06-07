17e749e8ca203ea1a2cb9eceef7e8ff078b741bb
Facundo Guerrero <facundo.guerrero@despegar.com>
2019-06-06 21:10:30 -0300

    modifies message

--- src/util/slack/SlackHelper.py
+++ src/util/slack/SlackHelper.py
@@ -1,13 +1,14 @@
 import datetime
 
 from slack import WebClient
+
+from src.db.dao.CandidatesFollowersDAO import CandidatesFollowersDAO
 from src.db.dao.RawFollowerDAO import RawFollowerDAO
 from src.db.dao.RawTweetDAO import RawTweetDAO
 from src.util.meta.Singleton import Singleton
 
 
 class SlackHelper(metaclass=Singleton):
-
     web_client = WebClient('xoxb-624537791124-655373675696-ViP74LIGog1aXvG1OUPMhCEa')
 
     @classmethod
@@ -15,7 +16,10 @@ class SlackHelper(metaclass=Singleton):
         yesterday = datetime.datetime.today() - datetime.timedelta(days=1)
         followers_updated = RawFollowerDAO().get_users_updated_since_date(yesterday)
         tweets_updated = RawTweetDAO().get_count()
-        message = f'Cantidad de tweets: {tweets_updated} \n Usuarios actualizados durante el día de ayer: {followers_updated}'
+        # new_followers = CandidatesFollowersDAO().get()
+        message = f'Cantidad de tweets descargados hasta el momento: {tweets_updated} \n ' \
+            f'Usuarios actualizados durante el día de ayer: {followers_updated} \n'
+        # f'Cantidad de usuarios nuevos capturados en la ultima corrida: {}'
         cls.post_message_to_channel(message)
 
     @classmethod
