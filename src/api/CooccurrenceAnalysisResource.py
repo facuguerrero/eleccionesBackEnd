7e8c3da9bf9e978735f6c5698202820c08e64db6
Rodrigo De Rosa <rderosa@fi.uba.ar>
2019-06-12 18:42:12 -0300

    minor fixes

--- src/api/CooccurrenceAnalysisResource.py
+++ src/api/CooccurrenceAnalysisResource.py
@@ -32,14 +32,16 @@ class CooccurrenceAnalysisResource(Resource):
         raw_start = query_params.get('start_date', None)
         start = CooccurrenceAnalysisResource._parse_raw(raw_start, 'start_date')
         raw_end = query_params.get('end_date', None)
-        end = CooccurrenceAnalysisResource._parse_raw(raw_end, 'end_date', nullable=False)
+        end = CooccurrenceAnalysisResource._parse_raw(raw_end, 'end_date', nullable=True)
         return start, end
 
     @staticmethod
-    def _parse_raw(raw_date, id, nullable=True):
+    def _parse_raw(raw_date, id, nullable=False):
         # Throw exception if date can't be parsed or if it is None
         try:
-            if not raw_date and not nullable: raise ValueError()
+            if raw_date is None:
+                if nullable: return None
+                else: raise ValueError()
             return datetime.strptime(raw_date, '%Y-%m-%d')
         except ValueError:
             raise WrongParametersError(id)
