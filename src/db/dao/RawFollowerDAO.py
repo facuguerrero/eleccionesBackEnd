aa90b991a4763e88afac78a48071710b6fa3bf4f
Facundo Guerrero <facundo.guerrero@despegar.com>
2019-06-19 14:36:32 -0300

    Final first Download
    Signed-off-by: Facundo Guerrero <facundo.guerrero@despegar.com>

--- src/db/dao/RawFollowerDAO.py
+++ src/db/dao/RawFollowerDAO.py
@@ -78,7 +78,8 @@ class RawFollowerDAO(GenericDAO, metaclass=Singleton):
             Returns {'id': 'last_update'}
         """
         date = datetime.datetime.today() - datetime.timedelta(days=21)
-        documents = self.get_with_limit({'downloaded_on': {'$lt': date}, 'is_private': False},
+        # 'downloaded_on': {'$lt': date},
+        documents = self.get_with_limit({'is_private': {'$exists': False}},
                                         {'_id': 1, 'downloaded_on': 1})
         followers_to_return = {}
         for document in documents:
