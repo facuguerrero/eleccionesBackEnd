c0d08cd8ff1fd0b9254a591cc68d83a77bb9da60
Facundo Guerrero <facundo.guerrero@despegar.com>
2019-06-20 21:15:49 -0300

    modifies slack query
    Signed-off-by: Facundo Guerrero <facundo.guerrero@despegar.com>

--- src/db/dao/RawFollowerDAO.py
+++ src/db/dao/RawFollowerDAO.py
@@ -71,7 +71,7 @@ class RawFollowerDAO(GenericDAO, metaclass=Singleton):
         return {document['_id'] for document in documents}
 
     def get_users_updated_since_date(self, date):
-        return self.get_count({'downloaded_on': {'$gt': date}, 'is_private': False}, {'_id': 1})
+        return self.get_count({'downloaded_on': {'$gt': date}}, {'_id': 1})
 
     def get_public_and_not_updated_users(self):
         """ Retrieve all the ids of the users that are not updated since one month catalogued as private.
