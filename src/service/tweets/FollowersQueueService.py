929604009ca0a723deb9b4791e1bf0c7e2124869
Facundo Guerrero <facundo.guerrero@despegar.com>
2019-06-24 21:46:07 -0300

    modifies private users
    Signed-off-by: Facundo Guerrero <facundo.guerrero@despegar.com>

--- src/service/tweets/FollowersQueueService.py
+++ src/service/tweets/FollowersQueueService.py
@@ -65,7 +65,7 @@ class FollowersQueueService(metaclass=Singleton):
 
     def add_last_downloaded_followers(self, private_users=200000):
         # TODO Borrar esto cuando se recorran todos los usuarios privados
-        date = datetime(2019, 6, 21, 15, 00, 00)
+        date = datetime(2019, 6, 24, 00, 00, 00)
         downloaded = RawFollowerDAO().get_with_limit({'$and': [
             {'is_private': True}, {'downloaded_on': {'$lt': date}}
         ]}, None, private_users)
