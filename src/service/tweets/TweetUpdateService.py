42ab50a83bf178e47ccbc032db9e15d444481c5a
Facundo Guerrero <facundo.guerrero@despegar.com>
2019-07-03 13:32:10 -0300

    contiguous private users
    Signed-off-by: Facundo Guerrero <facundo.guerrero@despegar.com>

--- src/service/tweets/TweetUpdateService.py
+++ src/service/tweets/TweetUpdateService.py
@@ -152,9 +152,9 @@ class TweetUpdateService:
         # If error code matches private_user or not_found
         if (error.error_code == ConfigurationManager().get_int('private_user_error_code') or
                 error.error_code == ConfigurationManager().get_int('not_found_user_error_code')):
-            # If throws this error 300 times in a row
+            # If throws this error 400 times in a row
             # Shut down this credential
-            if self.contiguous_private_users >= 300:
+            if self.contiguous_private_users >= 400:
                 self.shut_down_credential_and_notify('Too many private users. Shut down this credential',
                                                      "Muchos usuarios privados.")
             self.contiguous_private_users += 1
