d2eb8c904e127ec6e080e14fe075bbf4ba9a0bf2
Facundo Guerrero <facundo.guerrero@despegar.com>
2019-07-03 09:09:58 -0300

    Init
    Signed-off-by: Facundo Guerrero <facundo.guerrero@despegar.com>

--- src/db/dao/RawFollowerDAO.py
+++ src/db/dao/RawFollowerDAO.py
@@ -101,8 +101,8 @@ class RawFollowerDAO(GenericDAO, metaclass=Singleton):
         # Con un total de 1.250.435 usuarios que tienen tweets
         # Seteo ventana de 37 hs, lo que nos da 96k de base para actualizar + 31k por hora
 
-        # date = datetime.datetime.today() - datetime.timedelta(hours=55)
-        date = datetime.datetime(2019, 7, 1, 0, 0, 0)
+        date = datetime.datetime.today() - datetime.timedelta(hours=48)
+        # date = datetime.datetime(2019, 7, 1, 0, 0, 0)
         documents = self.aggregate([
             {"$match":
                 {"$and": [
