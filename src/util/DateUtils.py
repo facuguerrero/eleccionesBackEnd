658bc22128519481a47076c7abbd89630c30ad7f
Rodrigo De Rosa <rderosa@fi.uba.ar>
2019-07-07 12:40:52 -0300

    added DateUtils last_second_of_day method

--- src/util/DateUtils.py
+++ src/util/DateUtils.py
@@ -1,4 +1,4 @@
-from datetime import datetime
+from datetime import datetime, timedelta
 
 
 class DateUtils:
@@ -7,6 +7,11 @@ class DateUtils:
     def is_today(value):
         """ Determine if a given date is 'today'. """
         return datetime.today().date() == value.date()
+
+    @staticmethod
+    def last_second_of_day(value):
+        """ Returns a new datetime object from a datetime at 00:00:00 in the same day but at 23:59:59. """
+        return value + timedelta(days=1) - timedelta(seconds=1)
 
     @staticmethod
     def date_to_timestamp(date):
