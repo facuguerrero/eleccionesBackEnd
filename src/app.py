3083896705f0095833284b6d8c170ea932507370
Facundo Guerrero <facundo.guerrero@despegar.com>
2019-07-14 15:28:42 -0300

    only scheduler for support service
    Signed-off-by: Facundo Guerrero <facundo.guerrero@despegar.com>

--- src/app.py
+++ src/app.py
@@ -16,7 +16,6 @@ from src.api.RawFollowerResource import RawFollowerResource
 from src.api.TweetUpdatingResource import TweetUpdatingResource
 from src.db.Mongo import Mongo
 from src.db.db_initialization import create_indexes, create_base_entries, create_queue_entries
-from src.service.followers.FollowerSupportService import FollowerSupportService
 from src.service.tweets.TweetUpdateServiceInitializer import TweetUpdateServiceInitializer
 from src.util.logging.Logger import Logger
 from src.util.scheduling.Scheduler import Scheduler
@@ -59,7 +58,7 @@ def set_up_context(db_name, authorization, environment):
 def init_services():
     # This is not necessary
     # UserHashtagService().insert_hashtags_of_already_downloaded_tweets()
-    FollowerSupportService().init_update_support_follower()
+    # FollowerSupportService().init_update_support_follower()
     TweetUpdateServiceInitializer().initialize_tweet_update_service()
 
 
