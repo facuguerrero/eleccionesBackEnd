7f326beec7c2092e44bcd9490182b3fb8de27f16
Facundo Guerrero <facundo.guerrero@despegar.com>
2019-07-14 11:19:42 -0300

    Modifies Jobs
    Signed-off-by: Facundo Guerrero <facundo.guerrero@despegar.com>

--- src/util/scheduling/Scheduler.py
+++ src/util/scheduling/Scheduler.py
@@ -28,8 +28,8 @@ class Scheduler(metaclass=Singleton):
         self.scheduler.add_job(func=SlackHelper.send_server_status, trigger='cron', hour=8, minute=30, second=0)
         # Update followers support
         self.scheduler.add_job(func=FollowerSupportService.init_update_support_follower, trigger='cron',
-                               day_of_week='sun', hour=16)
+                               day_of_week='sat', hour=16)
         self.scheduler.add_job(func=FollowerSupportService.init_update_support_follower, trigger='cron',
-                               day_of_week='sun', hour=16)
+                               day_of_week='wed', hour=16)
         self.scheduler.start()
         atexit.register(lambda: self.scheduler.shutdown())
