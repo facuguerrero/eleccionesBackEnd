a53c0329ae2c0df8ce38123f7167dbc128abe746
Rodrigo De Rosa <rderosa@fi.uba.ar>
2019-07-28 18:47:53 -0300

    locks dict is now an expiring dict

--- src/util/concurrency/ConcurrencyUtils.py
+++ src/util/concurrency/ConcurrencyUtils.py
@@ -1,12 +1,12 @@
 from threading import Lock
-
+from expiringdict import ExpiringDict
 from src.util.meta.Singleton import Singleton
 
 
 class ConcurrencyUtils(metaclass=Singleton):
 
     def __init__(self):
-        self.locks = {}
+        self.locks = ExpiringDict(max_len=100000, max_age_seconds=86400)  # Locks last a full day
 
     def create_lock(self, lock_id):
         """ Create a new lock if it doesn't already exist one with the given id. """
