5379a8aa2a21dc58c50388a0202564d1afe9adc3
FacundoGuerrero <facundoiguerrero@gmail.com>
2019-08-27 10:48:38 -0300

    Final fix
    Signed-off-by: Facundo Guerrero <facundo.guerrero@despegar.com>

--- src/db/db_initialization.py
+++ src/db/db_initialization.py
@@ -2,6 +2,7 @@ from src.db.dao.CandidateDAO import CandidateDAO
 from src.db.dao.CooccurrenceGraphDAO import CooccurrenceGraphDAO
 from src.db.dao.RawFollowerDAO import RawFollowerDAO
 from src.db.dao.UserHashtagDAO import UserHashtagDAO
+from src.service.queue_followers.FollowersQueueService import FollowersQueueService
 
 
 def create_indexes():
@@ -18,7 +19,8 @@ def create_base_entries():
     """ Create all required entries. """
     CandidateDAO().create_base_entries()
 
-# def create_queue_entries():
-# """ Add followers to download's queue. """
+
+def create_queue_entries():
+    """ Add followers to download's queue. """
     # FollowersQueueService().add_followers_to_be_updated()
-# FollowersQueueService().add_last_downloaded_followers()
+    FollowersQueueService().add_last_downloaded_followers()
