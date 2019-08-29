a143158b5dd0d68ba0e272ace4e470c44cd72b8c
Rodrigo De Rosa <rderosa@fi.uba.ar>
2019-08-28 22:44:33 -0300

    added slack reporting

--- src/api/CooccurrenceAnalysisResource.py
+++ src/api/CooccurrenceAnalysisResource.py
@@ -6,6 +6,7 @@ from src.exception.NoHashtagCooccurrenceError import NoHashtagCooccurrenceError
 from src.exception.WrongParametersError import WrongParametersError
 from src.service.hashtags.CooccurrenceAnalysisService import CooccurrenceAnalysisService
 from src.util.ResponseBuilder import ResponseBuilder
+from src.util.slack.SlackHelper import SlackHelper
 
 
 class CooccurrenceAnalysisResource(Resource):
@@ -19,3 +20,4 @@ class CooccurrenceAnalysisResource(Resource):
         dates = [init + timedelta(days=i) for i in range((end-init).days + 1)]
         for date in dates:
             CooccurrenceAnalysisService.analyze(no_accumulate=True, last_day=date)
+            SlackHelper.post_message_to_channel(f'Finished cooccurrence graph generation for date {end}.')
