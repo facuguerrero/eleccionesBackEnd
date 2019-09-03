dc41a330ecc2c1c38b7a1992f6663ae1b4a0b84d
Facundo Guerrero <facundo.guerrero@despegar.com>
2019-09-03 10:28:05 -0300

    Fix
    Signed-off-by: Facundo Guerrero <facundo.guerrero@despegar.com>

--- src/app.py
+++ src/app.py
@@ -12,6 +12,7 @@ from src.api.PingResource import PingResource
 from src.api.TweetUpdatingResource import TweetUpdatingResource
 from src.db.Mongo import Mongo
 from src.db.db_initialization import create_indexes, create_base_entries, create_queue_entries
+from src.service.topics.UserTopicService import UserTopicService
 from src.service.tweets.TweetUpdateServiceInitializer import TweetUpdateServiceInitializer
 from src.util.logging.Logger import Logger
 from src.util.scheduling.Scheduler import Scheduler
@@ -53,7 +54,7 @@ def init_services():
     # UserHashtagService().insert_hashtags_of_already_downloaded_tweets()
     # UserTopicService().init_update_support_follower()
     # FollowerSupportService().init_update_support_follower()
-    # UserTopicService().init_update_support_follower()
+    UserTopicService().init_update_support_follower()
     TweetUpdateServiceInitializer().initialize_tweet_update_service()
 
 
