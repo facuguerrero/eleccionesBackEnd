8054902c8793454e2410317f122368c496149a15
Rodrigo De Rosa <rderosa@fi.uba.ar>
2019-09-05 17:15:35 -0300

    removed single day and accumulate topic calculation, changed 7 days to 28¿

--- src/api/CooccurrenceAnalysisResource.py
+++ src/api/CooccurrenceAnalysisResource.py
@@ -1,11 +1,7 @@
 from datetime import datetime, timedelta
-from flask import request
 from flask_restful import Resource
 
-from src.exception.NoHashtagCooccurrenceError import NoHashtagCooccurrenceError
-from src.exception.WrongParametersError import WrongParametersError
 from src.service.hashtags.CooccurrenceAnalysisService import CooccurrenceAnalysisService
-from src.util.ResponseBuilder import ResponseBuilder
 from src.util.slack.SlackHelper import SlackHelper
 
 
@@ -19,5 +15,5 @@ class CooccurrenceAnalysisResource(Resource):
         end = datetime.combine(datetime.strptime('2019-08-27', '%Y-%m-%d').date(), datetime.min.time())
         dates = [init + timedelta(days=i) for i in range((end-init).days + 1)]
         for date in dates:
-            CooccurrenceAnalysisService.analyze(no_accumulate=True, last_day=date)
+            CooccurrenceAnalysisService.analyze(last_day=date)
             SlackHelper.post_message_to_channel(f'Finished cooccurrence graph generation for date {end}.')
