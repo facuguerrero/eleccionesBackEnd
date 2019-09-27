4324228714c0636944f3c42ffaa2085143a369f0
Rodrigo De Rosa <rderosa@fi.uba.ar>
2019-09-27 15:55:42 -0300

    more debugging tools

--- src/util/concurrency/AsyncThreadPoolExecutor.py
+++ src/util/concurrency/AsyncThreadPoolExecutor.py
@@ -15,14 +15,16 @@ class AsyncThreadPoolExecutor:
         return self._run(executable, args_list, multiple=True)
 
     def _run(self, executable, args_list, multiple=False):
-        Logger(self.__class__.__name__).info("Starting asynchronous thread pool.")
-        max_workers = ConfigurationManager().get_int("max_pool_workers")
+        Logger(self.__class__.__name__).info('Starting asynchronous thread pool.')
+        max_workers = ConfigurationManager().get_int('max_pool_workers')
         with ThreadPoolExecutor(max_workers=max_workers) as executor:
+            Logger(self.__class__.__name__).info('Inside ThreadPoolExecutor context.')
             # TODO: This could be avoided but all single-param calls should send a list of one element
             if multiple: futures = self.__create_futures_multiple_args(executor, executable, args_list)
             else: futures = self.__create_futures(executor, executable, args_list)
+            Logger(self.__class__.__name__).info('Futures created.')
             results = [future.result() for future in as_completed(futures)]
-        Logger(self.__class__.__name__).info("Finished executing tasks in asynchronous thread pool.")
+        Logger(self.__class__.__name__).info('Finished executing tasks in asynchronous thread pool.')
         return results
 
     @staticmethod
