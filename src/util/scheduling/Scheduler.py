70ee4f7901633aa4b95eae30516027d99080226f
Facundo Guerrero <facundo.guerrero@despegar.com>
2019-11-05 10:23:34 -0300

    Listo
    Signed-off-by: Facundo Guerrero <facundo.guerrero@despegar.com>

--- src/util/scheduling/Scheduler.py
+++ src/util/scheduling/Scheduler.py
@@ -29,14 +29,14 @@ class Scheduler(metaclass=Singleton):
                                minute=0, second=0)
 
         # Adds not updated followers
-        self.scheduler.add_job(func=FollowersQueueService().add_not_updated_followers_1, trigger='cron', hour=21,
-                               minute=0, second=0)
-        self.scheduler.add_job(func=FollowersQueueService().add_not_updated_followers_2, trigger='cron', hour=1,
-                               minute=0, second=0)
-        self.scheduler.add_job(func=FollowersQueueService().add_not_updated_followers_1, trigger='cron', hour=11,
-                               minute=0, second=0)
-        self.scheduler.add_job(func=FollowersQueueService().add_not_updated_followers_2, trigger='cron', hour=15,
-                               minute=0, second=0)
+        # self.scheduler.add_job(func=FollowersQueueService().add_not_updated_followers_1, trigger='cron', hour=21,
+        #                       minute=0, second=0)
+        # self.scheduler.add_job(func=FollowersQueueService().add_not_updated_followers_2, trigger='cron', hour=1,
+        #                       minute=0, second=0)
+        # self.scheduler.add_job(func=FollowersQueueService().add_not_updated_followers_1, trigger='cron', hour=11,
+        #                       minute=0, second=0)
+        # self.scheduler.add_job(func=FollowersQueueService().add_not_updated_followers_2, trigger='cron', hour=15,
+        #                       minute=0, second=0)
 
         # Analyze cooccurrence at 00:01:00 every day
         self.scheduler.add_job(func=CooccurrenceAnalysisService.analyze, trigger='cron', hour=0, minute=1, second=0)
